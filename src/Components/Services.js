307cdc02e55a2bd7899304651959b3cc4b41fe31
Daniel <dsedaca@hotmail.com>
2024-05-09 12:31:47 -0400

    first commit

---
+++ src/Components/Services.js
@@ -0,0 +1,12 @@
+import Service from './Service'
+import Title from './Title'
+
+const Services = () => {
+  return (
+    <section className="section services" id="services">
+      <Title title="our" subTitle="services" />
+      <Service />
+    </section>
+  )
+}
+export default Services
